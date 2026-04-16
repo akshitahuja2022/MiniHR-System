5f55fc9066c0e8b12252d307e25b17f3177f2922
Akshit Ahuja <akshitahuja1322@gmail.com>
2026-04-16 19:10:31 +0530

    add admin middleware

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -35,3 +35,13 @@ export const isAuthenticated = async (req, res, next) => {
     });
   }
 };
+
+export const isAdmin = (req, res, next) => {
+  if (!req.user || req.user.role !== "Admin") {
+    return res.status(403).json({
+      message: "Access denied. Admin only",
+      success: false,
+    });
+  }
+  next();
+};
