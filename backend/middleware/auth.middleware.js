79b392c6a4449ee6db0a6732ed81e13ee6dc1cc9
Akshit Ahuja <akshitahuja1322@gmail.com>
2026-04-16 17:42:32 +0530

    Add middleware for fetched user profile

---
+++ backend/middleware/auth.middleware.js
@@ -0,0 +1,37 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import UserModel from "../models/user.model.js";
+
+dotenv.config();
+
+export const isAuthenticated = async (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+
+    if (!token) {
+      return res.status(401).json({
+        message: "Unauthorized - No token",
+        success: false,
+      });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await UserModel.findById(decoded.id).select("-password");
+
+    if (!user) {
+      return res.status(401).json({
+        message: "User not found",
+        success: false,
+      });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).json({
+      message: "Invalid token",
+      success: false,
+    });
+  }
+};
