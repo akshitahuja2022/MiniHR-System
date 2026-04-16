528b701ad257845f677ff034eede82e0ad59c4f8
Akshit Ahuja <akshitahuja1322@gmail.com>
2026-04-16 22:14:17 +0530

    add admin routes

---
+++ backend/routes/admin.route.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { isAdmin, isAuthenticated } from "../middleware/auth.middleware.js";
+import {
+  approveLeave,
+  getAllAttendence,
+  getAllUsers,
+  rejectLeave,
+} from "../controllers/admin.controller.js";
+
+const adminRouter = express.Router();
+
+adminRouter.get("/users", isAuthenticated, isAdmin, getAllUsers);
+adminRouter.put("/leave/approve/:id", isAuthenticated, isAdmin, approveLeave);
+adminRouter.put("/leave/reject/:id", isAuthenticated, isAdmin, rejectLeave);
+
+adminRouter.get("/all", isAuthenticated, isAdmin, getAllAttendence);
+
+export default adminRouter;
