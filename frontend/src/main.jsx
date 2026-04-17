1c367e0cdc8e3bcfcd5b4c4254328d8d44f75cbb
Akshit Ahuja <akshitahuja1322@gmail.com>
2026-04-17 11:36:28 +0530

    add leave context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,14 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
 import AuthContextProvider from "./Context/AuthContextProvider.jsx";
+import LeaveContextProvider from "./Context/LeaveContextProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AuthContextProvider>
-      <App />
+      <LeaveContextProvider>
+        <App />
+      </LeaveContextProvider>
     </AuthContextProvider>
   </BrowserRouter>,
 );
