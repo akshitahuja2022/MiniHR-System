8ccfc8a7660ebde8c18d27b7a61cbbe174d97f1a
Akshit Ahuja <akshitahuja1322@gmail.com>
2026-04-17 16:26:34 +0530

    remove unused line

--- backend/routes/admin.route.js
+++ backend/routes/admin.route.js
@@ -13,7 +13,6 @@ const adminRouter = express.Router();
 adminRouter.get("/users", isAuthenticated, isAdmin, getAllUsers);
 adminRouter.put("/leave/approve/:id", isAuthenticated, isAdmin, approveLeave);
 adminRouter.put("/leave/reject/:id", isAuthenticated, isAdmin, rejectLeave);
-
 adminRouter.get("/all", isAuthenticated, isAdmin, getAllAttendence);
 adminRouter.get("/leaves", isAuthenticated, isAdmin, getAllLeaves);
 export default adminRouter;
