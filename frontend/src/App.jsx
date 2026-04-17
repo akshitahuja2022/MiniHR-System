7bd8a4a8da972012d0610ee1c275ecd14d6b9cc5
Akshit Ahuja <akshitahuja1322@gmail.com>
2026-04-17 11:36:15 +0530

    add the routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,11 @@ import Home from "./pages/Home";
 import LoginPage from "./pages/Login";
 import SignupPage from "./pages/Signup";
 import { ToastContainer } from "react-toastify";
+import EmployeeDashboard from "./pages/EmployeeDashboard";
+import ApplyLeaveForm from "./Components/Employee/ApplyLeaveForm";
+import YourLeaves from "./Components/Employee/YourLeaves";
+import EditLeaveForm from "./Components/Employee/EditLeaveForm";
+
 const App = () => {
   return (
     <>
@@ -12,6 +17,10 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/dashboard" element={<EmployeeDashboard />} />
+        <Route path="/leaveApply" element={<ApplyLeaveForm />} />
+        <Route path="/yourLeaves" element={<YourLeaves />} />
+        <Route path="/updateLeave/:id" element={<EditLeaveForm />} />
       </Routes>
       <ToastContainer />
     </>
